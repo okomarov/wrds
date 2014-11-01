b8e73fe4969726b937b907dba14de495c054ba26
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-11-01 23:52:22 +0000

    [Change] sas2csv.sas: avoid proc export
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- sas2csv.m
+++ sas2csv.m
@@ -1,4 +1,4 @@
-function sas2csv(wrds, datasetname, outfile)
+function sas2csv(wrds, libdataname, outfile)
 % addpath .\external\ssh2\
 % wrds = ssh2_config('wrds.wharton.upenn.edu','olegkoma',
 % Import sas command
@@ -9,7 +9,8 @@ fclose(fid);
 % Create temp name 
 tmpfile = sprintf('~/tmp/f%s.csv',strrep(char(java.util.UUID.randomUUID),'-','_'));
 % Fill in dataset and temp file names
-sascmd = sprintf(str,  datasetname, tmpfile, datasetname, tmpfile);
+tmp = regexp(libdataname, '\.','split');
+sascmd = sprintf(str, tmp{:}, libdataname, tmpfile);
 
 % Replace
 sascmd = regexprep(sascmd,'\*[^\n\r]*[\n\r]*','');      % strip comments
@@ -26,5 +27,5 @@ cmd = sprintf(['rm tmp/sas2csv.sas;'...                 % Delete
 
 % Execute through ssh
 ssh2_command(wrds,cmd,1)
-
+% printf "@ -\n@=text.csv\n" | zipnote -w myfile.zip
 end
--- sas2csv.sas
+++ sas2csv.sas
@@ -1,15 +1,20 @@
-* Taken from https://communities.sas.com/message/185605#185605;
+* Taken from https://communities.sas.com/message/185633#185633
 
-* Write header line first;
-PROC EXPORT DATA= %s (OBS=0)
-            OUTFILE= "%s"
-            DBMS=CSV REPLACE;
-     PUTNAMES=YES;
-RUN;
+* get headers
+proc sql noprint;
+  select '"'||trim(name)||'"'
+    into :names
+      separated by " ','"
+        from dictionary.columns
+          where libname eq "%s" and
+                memname eq "%s"
+  ;
+quit;
 
-* Write the data;
-DATA _NULL_;
-   SET %s;
-   FILE  "%s" MOD DSD DLM=',' LRECL=1000000;
-   PUT (_ALL_) (:);
-RUN;
+* write the data;
+data _null_;
+   set %s;
+   file "%s" mod dsd dlm=',' lrecl=1000000;
+   if _n_ eq 1 then put &names.;
+   put (_all_) (+0);   
+run;
