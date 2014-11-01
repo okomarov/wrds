a75d22bd5feff7d45843975538c6053f141e90fe
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-11-01 21:54:52 +0000

    [Change] sas2csv.m, creates csv
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- sas2csv.m
+++ sas2csv.m
@@ -1,9 +1,30 @@
-function sas2csv(dtname)
+function sas2csv(wrds, datasetname, outfile)
+% addpath .\external\ssh2\
+% wrds = ssh2_config('wrds.wharton.upenn.edu','olegkoma',
 % Import sas command
 fid = fopen('sas2csv.sas');
 str = fread(fid,'*char')';
 fclose(fid);
-% Fill in dataset and temp file
-str = sprintf(str,  dtname, outfile, dtname, outfile);
+
+% Create temp name 
+tmpfile = sprintf('~/tmp/f%s.csv',strrep(char(java.util.UUID.randomUUID),'-','_'));
+% Fill in dataset and temp file names
+sascmd = sprintf(str,  datasetname, tmpfile, datasetname, tmpfile);
+
+% Replace
+sascmd = regexprep(sascmd,'\*[^\n\r]*[\n\r]*','');      % strip comments
+sascmd = regexprep(sascmd,'[ \t]*',' ');                % multiple spaces to one
+sascmd = regexprep(sascmd,'[\n\r]*','\\n');             % newlines to literal \n
+sascmd = regexprep(sascmd,'''','\\047');                % single quote ' to octal representation \047
+
+% Build command
+% mkdir -p tmp
+cmd = sprintf(['rm tmp/sas2csv.sas;'...                 % Delete 
+               'touch "tmp/sas2csv.sas";',...           % Create
+               'printf ''%s'' > tmp/sas2csv.sas;',...   % Write sas command
+               'sas tmp/sas2csv.sas'],sascmd);          % Execute sas
+
+% Execute through ssh
+ssh2_command(wrds,cmd,1)
 
 end
