56924cf36a72c7518f602738d588a0608a27189b
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-06-15 17:16:06 +0100

    Retrive sas data set info

---
+++ @wrds/getDatasetInfo.m
@@ -0,0 +1,48 @@
+function info = getDatasetInfo(wrds, libdataname)
+% GETDATASETNAMES Retrieve dataset names within a library
+%
+%    INFO = GETDATASETINFO(WRDS, LIBDATANAME) LIBDATANAME should be a 
+%                              string with the SAS library and dataset in 
+%                              the format <libref>.<data set>, 
+%                              e.g. 'CRSPA.MSI'.
+%               
+% See also: WRDS, GETLIBREFS, GETDATASETNAME
+
+% Library and datasetname
+tmp    = regexp(libdataname, '\.','split');
+libref = tmp{1};
+dtname = tmp{2};
+
+% Sanitize input
+try
+    allLib  = wrds.getLibrefs;
+    [~,pos] = ismember(libref, allLib);
+    libref  = allLib{pos};
+catch ME
+end
+
+if wrds.isVerbose, fprintf('Retrieving dataset info for ''%s''.\n', libdataname), end
+
+% SAS command
+sascmd = sprintf(['FILENAME out "~/tmp/cmd.lst";',...
+    'PROC PRINTTO print=out;',...
+    'RUN;',...
+    'PROC DATASETS LIBRARY=%s NOLIST;',...
+    'CONTENTS DATA=%s;',...
+    'RUN;'],...
+    libref, dtname);
+% UNIX command
+cmd    = sprintf(['touch "~/tmp/cmd.sas";',...              % Create file
+    'printf ''%s'' > ~/tmp/cmd.sas;',...                    % Write sas command
+    'sas ~/tmp/cmd.sas -log ~/tmp/cmd.log;',...             % Execute sas
+    'cat ~/tmp/cmd.lst;'],...                               % Print file
+    sascmd);
+
+% Execute through ssh
+[~,result] = wrds.cmd(cmd);
+info       = char(result);
+
+% Cleanup
+wrds.cmd('rm ~/tmp/cmd.*');
+
+end
