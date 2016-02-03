89443fac734945fdeafbc92cfc5a106891e3c5b3
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-02-03 14:46:11 +0000

    minor help edit

--- @wrds/getDataset.m
+++ @wrds/getDataset.m
@@ -1,12 +1,13 @@
 function getDataset(wrds, libdataname, outfile)
 % GETDATASET Download WRDS data set as zipped CSV
-%   GETDATASET(CONN, LIBDATANAME)
+%   GETDATASET(CONN, LIBDATANAME, [OUTFILE])
 %       Where CONN should be a valid WRDS connection and
 %       LIBDATANAME should be a string with the SAS library and
 %       dataset in the format <libref>.<data set>,
 %       e.g. 'CRSPA.MSI'.
 %
-%   SAS2CSV(..., OUTFILE)
+%       Specify where to save the downloaded dataset in OUTFILE.
+%       It defaults to fullfile(cd,  data, '<libref>.<data set>.zip').
 %
 % See also: GETDATASETINFO, GETFILE, WRDS, UNZIP, CSVREAD, READTABLE
 if nargin < 3, outfile = [libdataname '.zip']; end
