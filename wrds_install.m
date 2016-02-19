ce6f76ba1f04de0547610c5169f2e0e140b4e2b7
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-02-19 15:09:43 +0000

    fix #03, wrds_install download passfield submodule

--- wrds_install.m
+++ wrds_install.m
@@ -1,3 +1,4 @@
+function wrds_install()
 % WRDS_INSTALL Install the WRDS API
 
 % Add path of this folder
@@ -6,7 +7,21 @@ addpath(p)
 
 % Add path to ssh2
 addpath(fullfile(p,'external','ssh2'))
-addpath(fullfile(p,'external','passfield'))
+passfield_path = fullfile(p,'external','passfield');
+
+if isempty(dir(fullfile(passfield_path,'*.m')))
+    try
+        fname = unzip('https://github.com/okomarov/passfield/archive/master.zip',tempdir());
+        copyfile(fileparts(fname{1}), passfield_path)
+        addpath(passfield_path)
+    catch
+        warning(['could not install the passfield() dependency.\n\n', ...
+                 'For manual installation:\n',...
+                 '\t1) download the .zip file from <a href="https://github.com/okomarov/passfield/">github.com/okomarov/passfield</a>;\n',...
+                 '\t2) unpack and copy the content of the folder into ''%s'';\n',...
+                 '\t3) run addpath(''%s'').\n'],passfield_path,passfield_path)
+    end
+end
 
 % Save path
 % savepath
@@ -14,4 +29,5 @@ addpath(fullfile(p,'external','passfield'))
 % Make data path
 if ~exist(fullfile(p,'data'),'dir')
     mkdir(fullfile(p,'data'))
+end
 end
